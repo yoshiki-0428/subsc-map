e640d9852ada47def0d3210d8230c946b2595628
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-12-26 11:55:53 +0900

    [fix] image url

--- src/utils/get-ogp-image.js
+++ src/utils/get-ogp-image.js
@@ -1,5 +1,5 @@
 const OGP_TEMPLATE = 'https://res.cloudinary.com/dzaa4buiw/image/upload/l_text:Sawarabi%20Gothic_50_bold:';
-const OGP_SETTING = ',co_rgb:FFF,w_1000,c_fit,x_0,y_0/v1608303493/minna_subsc_back_empty_black.png';
+const OGP_SETTING = ',co_rgb:FFF,w_1000,c_fit,x_0,y_0/v1608303493/web/minna_subsc_back_empty_black.png';
 
 const getOgpImage = (text) => OGP_TEMPLATE + text + OGP_SETTING;
 export default getOgpImage;
